738b5f715d3d078c76078466aa5c2185d3f6b1e4
Divya Kaushik <kaushikdivya16@gmail.com>
2015-07-29 16:18:15 -0400

    trying something

---
+++ projPython/ex5.1.py
@@ -0,0 +1,5 @@
+userCount = 6
+#print "Users connected:" + userCount
+print "Users connected:",userCount # space always comes with used comma
+print "Users connected:%s" %( userCount ) # no space comes with %
+print "Users connected:%s" %( userCount, ) # no space comes with %
---
+++ projPython/ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Divya Kaushik'
+my_age = 28 #not a lie
+my_height = 5 #foot
+my_weight = 126 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "let's talk about %s." % my_name
+print "she's %d foot tall." % my_height
+print "she's %d pounds heavy" % my_weight
+print "Actually that's not too heavy."
+print "she's got %s eyes and %s hair." % ( my_eyes , my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+#this line is tricky, try to get it exactly right
+
+print "if I add %d, %d and %d I get %d." % ( my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+print "hey %s there" % "you"
